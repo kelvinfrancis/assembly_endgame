c6d845b1cd78e0c4e37ad7a1f1d797ce3a3953cf
Kelvin Moquete <kelvinmoquete0597@gmail.com>
2025-10-20 20:15:47 -0400

    fix: remove extra space in the header title

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,7 @@ function App() {
   return (
     <>
       <header>
-        <h1>Assembly: Endgame </h1>
+        <h1>Assembly: Endgame</h1>
         <p>
           Guess the word in under 8 attempts to keep the programming world safe
           from Assembly!
